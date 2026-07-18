74af36c22cdafb42b43761f9b569d8dc89aeeaaf
vikassagar-creator <bvikassagar@gmail.com>
2026-07-18 23:11:17 +0530

    improvements in index and result

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template , request
 import requests
-
-
+import datetime
 
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
 
 app = Flask(__name__)
 
@@ -16,56 +15,41 @@ def home():
     
     return render_template('index.html')
     
-@app.route('/submit', methods =['POST'])
+@app.route("/submit", methods =['POST'])
 def submit():
-    
-   
     city = request.form['city'] 
     
-
     url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
     response = requests.get(url)
-
-
 
     data = response.json()
     if "error" in data:
         return render_template("error.html")
-    elif response.status_code == 200:
-        
+    local_time = datetime.datetime.strptime(data["location"]["localtime"], "%Y-%m-%d %H:%M")
+    formated_time = local_time.strftime("%a, %d %b %Y %I:%M %p")
     
-        location = data["location"]["name"]
-        temp = data["current"]["temp_c"]
-        humidity = data["current"]["humidity"]
-        condition = data["current"]["condition"]["text"]
-        icon = data["current"]["condition"]["icon"]
-    
-        return render_template(
+    weather = {
+        "city": data["location"]["name"],
+        "country": data["location"]["country"],
+        "region": data["location"]["region"],
+        "localtime": formated_time,
+
+        "temperature": data["current"]["temp_c"],
+        "feelslike": data["current"]["feelslike_c"],
+        "condition": data["current"]["condition"]["text"],
+        "icon": data["current"]["condition"]["icon"],
+
+        "humidity": data["current"]["humidity"],
+        "wind_speed": data["current"]["wind_kph"],
+        "pressure": data["current"]["pressure_mb"],
+        "visibility": data["current"]["vis_km"],
+        "uv_index": data["current"]["uv"],
+        "cloud": data["current"]["cloud"],
+    }
+    return render_template(
             "result.html",
-            city=city,
-            location=location,
-            temp=temp,
-            humidity=humidity,
-            condition=condition,
-            icon=icon
+            weather=weather
         )
-
-    
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -76,4 +60,4 @@ def submit():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
